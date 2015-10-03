3e57809a28c37443f92dbbab3d03bd80ec96523e
Stelmashenko-A <stelma_28@bk.ru>
2015-10-03 17:51:38 +0300

    Runer, Result, ITeste

---
+++ TestServer/ITester.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TestServer
+{
+    public interface ITester
+    {
+        void Test(Process proc, Dictionary<string, string> testCases,
+            out Result result);
+    }
+}
